00fb8d867a47095e86aa5609cec8b1a69a3980f7
Khang <trongkhang1304@gmail.com>
2024-11-11 17:11:45 +0700

    Update index.js

--- Fullstackecommercereactjswithadminsourcecode/client/src/Components/Header/index.js
+++ Fullstackecommercereactjswithadminsourcecode/client/src/Components/Header/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import Logo from "../../assets/images/logo.png";
+import Logo from "../../assets/images/logo.jpg";
 import Button from "@mui/material/Button";
 import CountryDropdown from "../CountryDropdown";
 import { FiUser } from "react-icons/fi";
